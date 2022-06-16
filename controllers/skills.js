d4f868fa264e232ca08e32e9740705fc9c5efe47
Kairo Ribeiro <kairo10@gmail.com>
2022-06-16 17:16:33 -0400

    part 2

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,10 +1,18 @@
-import { skills } from '../data/skill-data.js'
+import { Skill } from '../models/skill.js'
 
 function index (req, res) {
+  Skill.find({})
+  .then(skills => {
     res.render('skills/index', {
       skills: skills
     })
-  }
+  })
+  .catch(error => { // If there's an error, console.log it and redirect back home!
+    console.log(error)
+    res.redirect('/')
+  })
+
+}
 
   export {
     index
