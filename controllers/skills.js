dc932eefd3cbabfe48ad46407837790617f31846
Kairo Ribeiro <kairo10@gmail.com>
2022-06-16 23:24:44 -0400

    part 4

--- controllers/skills.js
+++ controllers/skills.js
@@ -4,7 +4,8 @@ function index (req, res) {
   Skill.find({})
   .then(skills => {
     res.render('skills/index', {
-      skills: skills
+      skills: skills,
+      time: req.time
     })
   })
   .catch(error => { // If there's an error, console.log it and redirect back home!
@@ -44,10 +45,22 @@ function show(req, res) {
   })
 }
 
+function deleteSkill(req, res) {
+  Skill.findByIdAndDelete(req.params.id)
+  .then(skill => {
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
 
   export {
     index,
     newSkill as new,
     create,
-    show
+    show,
+    deleteSkill as delete,
   }
