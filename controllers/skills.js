73025be6d0c654bb108cbe30cea629939e3374b5
Kairo Ribeiro <kairo10@gmail.com>
2022-06-16 22:00:36 -0400

    part 3-2

--- controllers/skills.js
+++ controllers/skills.js
@@ -31,9 +31,23 @@ function create(req, res) {
   })
 }
 
+function show(req, res) {
+  Skill.findById(req.params.id)
+  .then(skill => {
+    res.render('skills/show', {
+      skill: skill
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
 
   export {
     index,
     newSkill as new,
-    create
+    create,
+    show
   }
